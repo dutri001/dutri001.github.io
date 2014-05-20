3c131e84ba098dd9ed4012fb1e308e253d2a22d6
Loïc (from home) <loic.dutrieux@gmail.com>
2014-05-20 23:58:20 +0200

    Added one placeholder for Rompon with infowindow; there's a trick to get more than one using a loop in the js script

--- js/mapScript.js
+++ js/mapScript.js
@@ -1,11 +1,37 @@
 function initialize() {
+  var myLatlng1 = new google.maps.LatLng(44.771463, 4.721819);
+  // var myLatlng2 = new google.maps.LatLng(51.972203, 5.668239);
   var mapOptions = {
     zoom: 4,
-    center: new google.maps.LatLng(-33, 151),
-    disableDefaultUI: true
-  }
+    disableDefaultUI: true,
+    center: myLatlng1
+  };
   var map = new google.maps.Map(document.getElementById('map-canvas'),
                                 mapOptions);
+
+  var contentString = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<h2 id="firstHeading" class="firstHeading">Rompon, France</h2>'+
+      '<div id="bodyContent">'+
+      '<p>The village where I grew up</p>'+
+      '</div>'+
+      '</div>';
+
+  var infowindow = new google.maps.InfoWindow({
+      content: contentString
+  });
+
+  var marker = new google.maps.Marker({
+      position: myLatlng1,
+      map: map,
+      title: 'Rompon, France'
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.open(map,marker);
+  });
 }
+
 
 google.maps.event.addDomListener(window, 'load', initialize);
