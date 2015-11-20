6009786a3dc62cad7ab9af41e0449005fe428c3f
Loïc <loic.dutrieux@gmail.com>
2015-11-20 17:43:28 +0100

    Wrote a simple json file of events

--- assets/js/timeLineEvents.js
+++ assets/js/timeLineEvents.js
@@ -4,10 +4,12 @@ var timeEvents = {
         "events": [
         {
             "start_date": {
-                "year":         "1987"
+                "year":         "1987",
+                "month": "11"
             },
             "end_date": {
-                "year":         "2005"
+                "year":         "2005",
+                "month": "9"
             },
             "media": {
                 "image": "imgs/loic.jpg",
@@ -15,13 +17,151 @@ var timeEvents = {
             },
             "text": {
                 "headline": "Rompon, Ardèche, France",
-                "text": "I grew up in Ardèche district in the South of France. "
+                "text": "Everything started in the South of France, in the small village of Rompon, where I grew up."
             },
             "coords" : {
-                "lng": 45,
-                "lat": 40
+                "lng": 4.72,
+                "lat": 44.76
             },
             "city" : "Rompon"
+        },
+        {
+            "start_date": {
+                "year":         "2005",
+                "month": "09"
+            },
+            "end_date": {
+                "year":         "2007",
+                "month": "09"
+            },
+            "media": {
+                "image": "imgs/loic.jpg",
+                "thumbnail": "imgs/loic.jpg"
+            },
+            "text": {
+                "headline": "Digne-les-Bains, France",
+                "text": "After graduating from high school, I joined university of Digne-les-Bains, where I studied biology and environmental engineering. Besides staying close to the mountains and going mountainering and skying litteraly every week, I learnt about a wide array of topics there, ranging from molecular biology to waste water management and from geology to river management; always in an Mediterraneo-alpine context."
+            },
+            "coords" : {
+                "lng": 6.236,
+                "lat": 44.09
+            },
+            "city" : "Digne-les-Bains"
+        },
+        {
+            "start_date": {
+                "year":         "2007",
+                "month": "09"
+            },
+            "end_date": {
+                "year":         "2011",
+                "month": "09"
+            },
+            "media": {
+                "image": "imgs/loic.jpg",
+                "thumbnail": "imgs/loic.jpg"
+            },
+            "text": {
+                "headline": "Angers, France",
+                "text": "I then joined an Agriculture engineering university from which I optained my first master degree, specialized in Agronomy and Landscape ecology"
+            },
+            "coords" : {
+                "lng": 0.554,
+                "lat": 47.473
+            },
+            "city" : "Angers"
+        },
+        {
+            "start_date": {
+                "year":         "2008",
+                "month": "06"
+            },
+            "end_date": {
+                "year":         "2008",
+                "month": "09"
+            },
+            "media": {
+                "image": "imgs/loic.jpg",
+                "thumbnail": "imgs/loic.jpg"
+            },
+            "text": {
+                "headline": "Central Farm, Belize",
+                "text": "In 2008 I spent 3 months in Belize for an intership. I was working with the ministry of agriculture; most of the time touring with the agriculture extension team and designing an irrigation system for pastures the remaining part of the time"
+            },
+            "coords" : {
+                "lng": -89.04,
+                "lat": 17.1785
+            },
+            "city" : "Central Farm"
+        },
+        {
+            "start_date": {
+                "year":         "2009",
+                "month": "01"
+            },
+            "end_date": {
+                "year":         "2016",
+                "month": "04"
+            },
+            "media": {
+                "image": "imgs/loic.jpg",
+                "thumbnail": "imgs/loic.jpg"
+            },
+            "text": {
+                "headline": "Wageningen, The Netherlands",
+                "text": "Since 2009 I have been living, studying and working in the Netherlands. Most of my stay here is related to Wageningen University. After finishing my second master in water and land management, I started a PhD in remote sensing which I expect to finish in April 2016.",
+            },
+            "coords" : {
+                "lng": 5.66,
+                "lat": 51.966
+            },
+            "city" : "Wageningen"
+        },
+        {
+            "start_date": {
+                "year":         "2010",
+                "month": "07"
+            },
+            "end_date": {
+                "year":         "2010",
+                "month": "11"
+            },
+            "media": {
+                "image": "imgs/loic.jpg",
+                "thumbnail": "imgs/loic.jpg"
+            },
+            "text": {
+                "headline": "Niono, Mali",
+                "text": "As part of my master thesis, I went to Mali in 2010. I researched what were the parameters affecting water productivity in irrigated rice systems research on irrigated rice combining field inventories and remote sensing"
+            },
+            "coords" : {
+                "lng": -5.986,
+                "lat": 14.25
+            },
+            "city" : "Niono"
+        },
+        {
+            "start_date": {
+                "year":         "2010",
+                "month": "11"
+            },
+            "end_date": {
+                "year":         "2010",
+                "month": "12"
+            },
+            "media": {
+                "image": "imgs/loic.jpg",
+                "thumbnail": "imgs/loic.jpg"
+            },
+            "text": {
+                "headline": "Cotonou, Benin",
+                "text": "Following my master thesis field work period, I flew to Benin to analyze, together with researchers from the Africa Rice center, the data I collected"
+            },
+            "coords" : {
+                "lng": 2.433,
+                "lat": 6.366
+            },
+            "city" : "Cotonou"
         },
     ]
 }
