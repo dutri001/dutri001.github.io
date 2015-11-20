196a7e822c3118e0e28b344ca54667c61ca7315f
Loïc <loic.dutrieux@gmail.com>
2015-11-20 16:45:42 +0100

    Transfered timeMap to webpage

---
+++ assets/js/timeLineEvents.js
@@ -0,0 +1,27 @@
+var timeEvents = {
+        "scale": "human",
+
+        "events": [
+        {
+            "start_date": {
+                "year":         "1987"
+            },
+            "end_date": {
+                "year":         "2005"
+            },
+            "media": {
+                "image": "imgs/loic.jpg",
+                "thumbnail": "imgs/loic.jpg"
+            },
+            "text": {
+                "headline": "Rompon, Ardèche, France",
+                "text": "I grew up in Ardèche district in the South of France. "
+            },
+            "coords" : {
+                "lng": 45,
+                "lat": 40
+            },
+            "city" : "Rompon"
+        },
+    ]
+}
